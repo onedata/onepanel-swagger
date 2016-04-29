1dd7d9dec656cd22f24b836c12cc95fc64d718ad
Bartosz Kryza <bkryza@gmail.com>
2016-04-29 11:21:55 +0200

    Added pathlist route generation

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Generate aggregate JSON file from YAML
-#docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-aggregator:1.2.0
-docker run --rm -v `pwd`:/swagger swagger-aggregator6
-
+docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-aggregator:1.4.0
 
 # Validate the JSON
-docker run --rm -v `pwd`:/swagger swagger-cli validate /swagger/swagger.json
+docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.2.0 validate /swagger/swagger.json
 
 # Generate the Cowboy server stub
-docker run --rm -v `pwd`:/swagger -t swagger-codegen2 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
+docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
 
 # Generate the 
-docker run --rm -v `pwd`:/swagger -t swagger-codegen2 generate -i ./swagger.json -l html -o ./generated/html
+docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l html -o ./generated/html
 
