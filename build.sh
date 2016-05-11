b8f8ff1246938b0919c3d1c219460ab5ab7906ba
Bartosz Kryza <bkryza@gmail.com>
2016-05-11 14:42:09 +0200

    Improved mustache templates

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.2.0 validate 
 # Generate the Cowboy server stub
 docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
 
+# Generate C# stub to get all moustache tempalte keywords
+#docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l csharp -o ./generated/csharp
+swagger-codegen-dbg generate -i ./swagger.json -l csharp -o ./generated/csharp -o tmp > model.json
+
 # Generate the 
 docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l html -o ./generated/html
 
