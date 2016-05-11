e7b4644274f59a01b49b4c46e00fd774df7f0c1f
Bartosz Kryza <bkryza@gmail.com>
2016-05-11 18:14:36 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.2.0 validate 
 docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
 
 # Generate C# stub to get all moustache tempalte keywords
-#docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l csharp -o ./generated/csharp
-swagger-codegen-dbg generate -i ./swagger.json -l csharp -o ./generated/csharp -o tmp > model.json
+#swagger-codegen-dbg generate -i ./swagger.json -l csharp -o ./generated/csharp -o tmp > model.json
 
 # Generate the 
 docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l html -o ./generated/html
