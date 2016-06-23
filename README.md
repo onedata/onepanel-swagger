1a19fe4c6e2e8f49748eaa0b51f25c85e104f0f3
Bartosz Kryza <bkryza@gmail.com>
2016-06-23 20:43:19 +0200

    Version integrated with gitbook generator

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Onepanel REST API defined using Swagger (http://swagger.io)
 
-This repo contains Swagger specification of Onepanel REST API.
+This repo contains Swagger specification of Oneprovider REST API.
 
+Onepanel is part of [Onedata](http://onedata.org), a distributed data management platform. Onepanel is an administrative service which enables management of other Onedata services including [Onezone](http://github.com/onedata/onezone) and [Oneprovider](http://github.com/onedata/oneprovider).
 
-Compiling:
-- Run ./build.sh from this directory and it should generate 'swagger.json' file
+For more details about Onepanel service please check (http://github.com/onedata/onepanel).
+
+Compiling (Using Onedata docker repository):
+```
+./build.sh
+```
+
+Compiling from scratch:
+```
+# Install Node.js dependencies
+npm install
+
+# Aggregate Yaml specification files into a single swagger.json file
+node resolve.js > swagger.json
+```
+
+After any of these steps you should have a complete `swagger.json` file, with specification of Onepanel REST API. The file can be used by Swagger [code generator](https://github.com/swagger-api/swagger-codegen) to produce example code for clients in various languages or viewed online using for instance [Swagger Online Editor](http://editor.swagger.io/).
--- build.sh
+++ build.sh
@@ -7,18 +7,20 @@ docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagge
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.5.0 validate /swagger/swagger.json
 
 # Generate the Cowboy server stub
-docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.0 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
+docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.2 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.0 generate -i ./swagger.json -l go -o ./generated/goclient
 
 # Generate C# stub to get all moustache tempalte keywords
 swagger-codegen-dbg generate -i ./swagger.json -l csharp -o ./generated/csharp -o tmp > model.json
 
 # Generate the 
-#docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.0 generate -i ./swagger.json -l html -o ./generated/html
+docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.0 generate -i ./swagger.json -l html -o ./generated/html
 
-#exit 0
 
 # Generate the static documentation
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-bootprint:1.5.0 swagger ./swagger.json generated/static
 
-#sed -n '/<body>/,/<\/body>/p' generated/static/index.html | sed -e '1s/.*<body>//' -e '$s/<\/body>.*//' -e 's/\/definitions\//definitions--/g' > generated/static/onepanel-static.html
+
+# Generate Markdown for direct Gitbook integration
+# The output from generated/gitbook should be copied to onedata-documentation/doc/advanced/rest/onezone folder
+docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-gitbook:1.2.0 convert -i ./swagger.json -d ./generated/gitbook -c ./gitbook.properties
