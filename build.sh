b781a354266a84fd62a9f92ed7f0f3e5721b0c4d
Bartosz Kryza <bkryza@gmail.com>
2016-08-29 13:15:56 +0200

    Fixed build script error handling

--- build.sh
+++ build.sh
@@ -6,32 +6,29 @@ docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagge
 # Validate the JSON
 output=$(docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.5.0 validate /swagger/swagger.json 2>&1)
 
-if [[ $output =~ .*Swagger\ schema\ validation\ failed.* ]]; then
+if [[ $output =~ .*SyntaxError.* ]] ; then
   echo "Generated swagger.json is not valid - check YAML sources:\n"
-  echo $output  
+  echo $output
   exit 1
 fi
 
 # Generate the Cowboy server stub
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.3 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
 
-# Fix generated files
+# Fix generated Erlang files
 ./fix_generated.py
 
-docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.0 generate -i ./swagger.json -l go -o ./generated/goclient
-
-# Generate C# stub to get all moustache template keywords
+# Generate C# stub to get all moustache tempalte keywords
 #swagger-codegen-dbg generate -i ./swagger.json -l csharp -o ./generated/csharp -o tmp > model.json
-
-
-# Generate the 
-docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.0 generate -i ./swagger.json -l html -o ./generated/html
-
 
 # Generate the static documentation
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-bootprint:1.5.0 swagger ./swagger.json generated/static
 
+#cp generated/static/index.html generated/static/oneprovider-static.html
+sed -n '/<body>/,/<\/body>/p' generated/static/index.html \
+  | sed -e '1s/.*<body>//' -e '$s/<\/body>.*//' -e 's/\/definitions\//definitions--/g' -e 's/<div class=\"container\"/<div class=\"container swagger\"/' \
+  > generated/static/onepanel-static.html
 
 # Generate Markdown for direct Gitbook integration
-# The output from generated/gitbook should be copied to onedata-documentation/doc/advanced/rest/onezone folder
+# The output from generated/gitbook should be copied to onedata-documentation/doc/advanced/rest/onepanel folder
 docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-gitbook:1.4.1 convert -i ./swagger.json -d ./generated/gitbook -c ./gitbook.properties
