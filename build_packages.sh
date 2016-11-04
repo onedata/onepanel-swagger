e223dbab8689ef76347935b0b2a34fdabe01574f
Bartosz Kryza <bkryza@gmail.com>
2016-11-04 16:07:44 +0100

    Added package builder

---
+++ build_packages.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+declare -A releases
+
+#
+# Maps branch names to release names
+#
+releases["release/3.0.0-rc9"]="3.0.0-rc9"
+releases["release/3.0.0-rc10"]="3.0.0-rc10"
+
+rm -rf packages
+for release_branch in "${!releases[@]}"; do
+    git checkout $release_branch
+    rm -rf generated
+
+    docker run --rm -e "CHOWNUID=${UID}" \
+        -v `pwd`:/swagger docker.onedata.org/swagger-aggregator:1.5.0
+    docker run --rm -e "CHOWNUID=${UID}" \
+        -v `pwd`:/swagger -t docker.onedata.org/bash-swagger-codegen:0.3.7 generate \
+        -i ./swagger.json -l bash -o ./generated/bash -c bash-config.json
+
+    mkdir -p "packages/bash/${releases[$release_branch]}"
+    cp generated/bash/onepanel-rest-cli \
+        "packages/bash/${releases[$release_branch]}/"
+    cp generated/bash/_onepanel-rest-cli \
+        "packages/bash/${releases[$release_branch]}/"
+    cp generated/bash/onepanel-rest-cli.bash-completion \
+        "packages/bash/${releases[$release_branch]}/"
+done
+
+git checkout master
