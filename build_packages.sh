40e2f3554f968c73b6d39d3ca57ae7e43f612db2
Bartosz Kryza <bkryza@gmail.com>
2017-02-14 18:35:28 +0100

    Updated swagger docker version

--- build_packages.sh
+++ build_packages.sh
@@ -18,7 +18,7 @@ for release_branch in "${!releases[@]}"; do
     docker run --rm -e "CHOWNUID=${UID}" \
         -v `pwd`:/swagger docker.onedata.org/swagger-aggregator:1.5.0
     docker run --rm -e "CHOWNUID=${UID}" \
-        -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:ID-6318f86f77 generate \
+        -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:ID-2fc8126ac8 generate \
         -i ./swagger.json -l bash -o ./generated/bash -c bash-config.json
 
     mkdir -p "packages/bash/${releases[$release_branch]}"
