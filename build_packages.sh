d9eb8397ca30d86bc5540c72c30743ea46093ea5
Bartek Kryza <bkryza@gmail.com>
2017-06-13 18:24:44 +0200

    Added rc15

--- build_packages.sh
+++ build_packages.sh
@@ -9,6 +9,7 @@ releases["release/3.0.0-rc9"]="3.0.0-rc9"
 releases["release/3.0.0-rc10"]="3.0.0-rc10"
 releases["release/3.0.0-rc11"]="3.0.0-rc11"
 releases["release/3.0.0-rc14"]="3.0.0-rc14"
+releases["release/3.0.0-rc15"]="3.0.0-rc15"
 
 rm -rf packages
 for release_branch in "${!releases[@]}"; do
