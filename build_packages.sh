03161263e4727ffe8d9afba87e47477ffc7b4292
Bartek Kryza <bkryza@gmail.com>
2017-07-10 19:37:56 +0200

    Added 17.06.0-beta5

--- build_packages.sh
+++ build_packages.sh
@@ -12,6 +12,7 @@ releases["release/3.0.0-rc14"]="3.0.0-rc14"
 releases["release/3.0.0-rc15"]="3.0.0-rc15"
 releases["release/3.0.0-rc16"]="3.0.0-rc16"
 releases["release/17.06.0-beta2"]="17.06.0-beta2"
+releases["release/17.06.0-beta5"]="17.06.0-beta5"
 
 rm -rf packages
 for release_branch in "${!releases[@]}"; do
