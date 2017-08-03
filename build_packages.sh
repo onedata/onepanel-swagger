0825dfca936f3695a16127ce8cb2f18263dfebbe
Bartek Kryza <bkryza@gmail.com>
2017-08-03 14:27:16 +0200

    Added 17.06.0-rc2 release

--- build_packages.sh
+++ build_packages.sh
@@ -13,6 +13,7 @@ releases["release/3.0.0-rc15"]="3.0.0-rc15"
 releases["release/3.0.0-rc16"]="3.0.0-rc16"
 releases["release/17.06.0-beta2"]="17.06.0-beta2"
 releases["release/17.06.0-beta6"]="17.06.0-beta6"
+releases["release/17.06.0-rc2"]="17.06.0-rc2"
 
 rm -rf packages
 for release_branch in "${!releases[@]}"; do
