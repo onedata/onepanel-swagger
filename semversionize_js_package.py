4a0ea9c7634074ae010614d4835a276d1f9f1f18
Jakub Liput <jakub.liput@gmail.com>
2024-01-17 16:03:21 +0100

    VFS-11617 Code clean; added LICENSE.txt

--- semversionize_js_package.py
+++ semversionize_js_package.py
@@ -19,18 +19,18 @@ version_re = re.compile(r'(\d+)\.(\d+)\.(\d+)')
 package_json_path = 'generated/javascript/package.json'
 
 if len(sys.argv) >= 2:
-  package_json_path = sys.argv[1]
+    package_json_path = sys.argv[1]
 
 with open(package_json_path, 'r+') as package_writer:
-  content = package_writer.read()
-  package_data = json.loads(content)
-  version_match = version_re.match(package_data['version'])
-  version_year = version_match.group(1)
-  version_month = version_match.group(2)
-  version_minor = version_match.group(3)
-  result_version = f'{int(version_year)}.{int(version_month)}.{int(version_minor)}'
-  package_data['version'] = result_version
-  new_content = json.dumps(package_data, indent='  ')
-  package_writer.seek(0)
-  package_writer.truncate()
-  package_writer.write(new_content)
+    content = package_writer.read()
+    package_data = json.loads(content)
+    version_match = version_re.match(package_data['version'])
+    version_year = version_match.group(1)
+    version_month = version_match.group(2)
+    version_minor = version_match.group(3)
+    result_version = f'{int(version_year)}.{int(version_month)}.{int(version_minor)}'
+    package_data['version'] = result_version
+    new_content = json.dumps(package_data, indent='  ')
+    package_writer.seek(0)
+    package_writer.truncate()
+    package_writer.write(new_content)
